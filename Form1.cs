4c217348a8f9d8201b3e3c9355a5545b1add2ef2
Nikhil Pandey <nikhil1.pande@yahoo.com>
2020-08-18 20:34:46 +0530

    including remaining test cases

--- Form1.cs
+++ Form1.cs
@@ -32,6 +32,8 @@ namespace Promotion_App
         [Test]
         public void TestMethod()
         {
+            // First Set of Test Cases and Statements
+
             int promotion1 = 1 * A;
             Assert.AreEqual(promotion1, 50);
             MessageBox.Show(promotion1.ToString());
@@ -41,6 +43,27 @@ namespace Promotion_App
             int promotion3 = 1 * C;
             Assert.AreEqual(promotion3, 20);
             MessageBox.Show(promotion3.ToString());
+            int Sum1 = promotion1 + promotion2 + promotion3;
+            Assert.AreEqual(Sum1,promotion1+promotion2+promotion3);
+
+
+            // New Set Of Statements and Test Cases
+
+
+            int secondpromotion1 = 5 * A;
+            int secondpromotion2 = 5 * B;
+            int secondpromotion3 = 1 * C;
+            int Sum2 = secondpromotion1 + secondpromotion2 + secondpromotion3;
+            Assert.AreEqual(Sum2, secondpromotion1 + secondpromotion2 + secondpromotion3);
+
+            // New Set Of Statements and Test Cases
+
+            int thirdPromotion1 = 3 * A;
+            int thirdPromotion2 = 5 * B;
+            int thirdPromotion3 = 1 * C;
+            int thirdPromotion4 = 1 * D;
+            int Sum3 = thirdPromotion1 + thirdPromotion2 + thirdPromotion3 + thirdPromotion4;
+            Assert.AreEqual(Sum3, thirdPromotion1 + thirdPromotion2 + thirdPromotion3 + thirdPromotion4);
         }
     }
 }
